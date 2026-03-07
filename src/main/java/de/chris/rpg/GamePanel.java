7456e828f1747d07f4fa545e80900c51096696db
Your Name <youexample.com>
2026-03-07 15:30:51 +0100

    Level 3: Fredolin joins

--- src/main/java/de/chris/rpg/GamePanel.java
+++ src/main/java/de/chris/rpg/GamePanel.java
@@ -1,12 +1,16 @@
 package de.chris.rpg;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class GamePanel extends JPanel implements Runnable {
     private int playerX = 100;
     private int playerY = 100;
     private KeyHandler keyH = new KeyHandler();
+    private BufferedImage playerImage;
 
     private Thread gameThread;
 
@@ -14,6 +18,15 @@ public class GamePanel extends JPanel implements Runnable {
         this.setBackground(Color.BLACK);
         this.addKeyListener(keyH);
         this.setFocusable(true);
+        getPlayerImage("fredolin");
+    }
+
+    private void getPlayerImage(String player) {
+        try {
+            playerImage = ImageIO.read(getClass().getResourceAsStream("/" + player + ".png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void startGameThread() {
@@ -24,18 +37,7 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
         while (gameThread != null) {
-            if (keyH.upPressed) {
-                this.playerY -= 4;
-            }
-            if (keyH.leftPressed) {
-                this.playerX -= 4;
-            }
-            if (keyH.downPressed) {
-                this.playerY += 4;
-            }
-            if (keyH.rightPressed) {
-                this.playerX += 4;
-            }
+            update();
             repaint();
             try {
                 Thread.sleep(16);
@@ -45,10 +47,27 @@ public class GamePanel extends JPanel implements Runnable {
         }
     }
 
+    private void update() {
+        if (keyH.upPressed) {
+            this.playerY -= 4;
+        }
+        if (keyH.leftPressed) {
+            this.playerX -= 4;
+        }
+        if (keyH.downPressed) {
+            this.playerY += 4;
+        }
+        if (keyH.rightPressed) {
+            this.playerX += 4;
+        }
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.RED);
-        g.fillRect(this.playerX, this.playerY, 50, 50);
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.drawImage(playerImage, this.playerX, this.playerY, 26, 50, null);
+        g2.dispose();
     }
 }
